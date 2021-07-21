32ce94a0c2a1c03dca33fb4931f80019cd525de8
SAMIR QULUZADEH <squluzade@std.beu.edu.az>
2021-07-21 22:00:48 +0400

    COMMIT

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
 import orderRouter from "./routers/orderRouter.js";
-// const path = require("path");
+const path = require("path");
 
 dotenv.config();
 const app = express();
@@ -26,10 +26,14 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("/frontend/build"));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API running!");
   });
 }
 const port = process.env.PORT || 5000;
