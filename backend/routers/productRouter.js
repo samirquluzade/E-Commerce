38d17da171feb02c2ccf30164e403de28a164ae6
SAMIR QULUZADEH <squluzade@std.beu.edu.az>
2021-08-07 20:44:09 +0400

    ALL FIXED

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -7,17 +7,17 @@ import { isAdmin, isAuth } from "../utils.js";
 const productRouter = express.Router();
 
 productRouter.get(
-  "/",
-  expressAsyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.send(products);
-  })
-);
-productRouter.get(
   "/seed",
   expressAsyncHandler(async (req, res) => {
     const createdProducts = await Product.insertMany(data.products);
     res.send({ createdProducts });
+  })
+);
+productRouter.get(
+  "/",
+  expressAsyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.send(products);
   })
 );
 productRouter.delete(
